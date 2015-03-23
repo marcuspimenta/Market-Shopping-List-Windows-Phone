4aab63060994c61b01e081e5b0b284a342dc9342
Marcus Pimenta <mvinicius.pimenta@gmail.com>
2015-03-23 13:30:28 -0300

    Fixed type language used in project

---
+++ Market Shopping List/Market Shopping List/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Market_Shopping_List.Resources;
+
+namespace Market_Shopping_List
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
